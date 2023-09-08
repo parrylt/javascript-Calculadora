c4e999d9b9a01e6693d4c2b9837e373a6cf862cd
Rômulo Junior <132522416+parrylt@users.noreply.github.com>
2023-09-08 20:53:56 -0300

    Add files via upload

---
+++ js/javinha.js
@@ -0,0 +1,32 @@
+﻿alert ("Calculadora");
+var numero= prompt ("Digite 1 para fazer adição, 2 para subtração, 3 para multiplicação e 4 para divisão.");
+
+
+
+var n1= prompt ("Digite o primeiro número: ");
+var n2 = prompt ("Digite o segundo número: ");
+var totalmais = parseFloat (n1) + parseFloat (n2);
+var totalmenos = parseFloat (n1) - parseFloat (n2);
+var totaldiv = parseFloat (n1) / parseFloat (n2);
+var totalmult = parseFloat (n1) * parseFloat (n2);
+
+if (numero==1)
+{
+	document.write ("O resultado da adição é: " + totalmais);
+}
+else if (numero==2)
+{
+	document.write ("O resultado da subtração é: " + totalmenos);
+}
+else if (numero==3)
+{
+	document.write ("O resultado da multiplicação é: " + totalmult);
+}
+else if (numero==4)
+{
+	document.write ("O resultado da divisão é: " + totaldiv);
+}
+else
+{
+	document.write("Opção inválida!!");
+}
